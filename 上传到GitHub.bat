e0849288ec1e77d1a7fddd03c6dd6b31573b2e9d
ZPP011 Dev <zpp011@local.dev>
2026-05-11 18:40:59 +0800

    auto update

---
+++ 上传到GitHub.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 65001 >nul
+cd /d "%~dp0"
+
+echo ==============================
+echo    正在上传 ZPP011 V36
+echo ==============================
+
+git add .
+git commit -m "auto update"
+git push
+
+echo.
+echo 上传完成！
+pause
